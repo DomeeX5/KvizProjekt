601e4fd5306f30d13d67e23a9e6b874d5e15770a
Dominik Zsolt Major <major.dominik.zsolt@petrik.hu>
2023-12-02 23:13:22 +0100

    helyes/helytelen válaszok javításra vár

--- src/lastpage.ts
+++ src/lastpage.ts
@@ -1,3 +1,4 @@
+import { RandomOrder } from "./randomOrder";
 const pointsParag = document.getElementById('points') as HTMLParagraphElement;
 const correctLabel = document.getElementById('correctLabel');
 const incorrectLabel1 = document.getElementById('incorrect1Label');
@@ -5,11 +6,79 @@ const incorrectLabel2 = document.getElementById('incorrect2Label');
 const correctInput = document.getElementById('correct') as HTMLInputElement;
 const incorrectInput1 = document.getElementById('incorrect1') as HTMLInputElement;
 const incorrectInput2 = document.getElementById('incorrect2') as HTMLInputElement;
+const app2Div = document.getElementById('app2')
 let points = parseInt(localStorage.getItem("points")!);
 let selectedData: string[] = JSON.parse(localStorage.getItem("selectedOptions")!);
+let quizQuestions: string[] = JSON.parse(localStorage.getItem("quizQuestions")!);
 console.log(selectedData);
+console.log(quizQuestions);
 pointsParag.textContent = `Pontszámod: ${points}/10`
+let k = 0;
 
 document!.addEventListener('DOMContentLoaded', () => {
+    for (let i = 0; i < selectedData.length; i++) {
+    const div = document.createElement('div')
+    const ul = document.createElement('ul')
+    const image = document.createElement('img')
+    image.classList.add('lastpageImage')
+    const li1 = document.createElement('li')
+    const li2 = document.createElement('li')
+    const li3 = document.createElement('li')
+    const input1 = document.createElement('input')
+    input1.type = 'radio'
+    input1.name = 'test'
+    const input2 = document.createElement('input')
+    input2.type = 'radio'
+    input2.name = 'test'
+    const input3 = document.createElement('input')
+    input3.type = 'radio'
+    input3.name = 'test'
+    const label1 = document.createElement('label')
+    const label2 = document.createElement('label')
+    const label3 = document.createElement('label')
     
+
+    
+    app2Div!.appendChild(div)
+    div.appendChild(image)
+    div.appendChild(ul)
+    ul.appendChild(li1)
+    ul.appendChild(li2)
+    ul.appendChild(li3)
+    li1.appendChild(input1)
+    li2.appendChild(input2)
+    li3.appendChild(input3)
+    li1.append(label1)
+    li2.appendChild(label2)
+    li3.appendChild(label3)
+    image.src = quizQuestions[k]
+    k++;
+    label1.textContent = quizQuestions[k]
+    k++;
+    label2.textContent = quizQuestions[k]
+    k++;
+    label3.textContent = quizQuestions[k]
+    k++;
+    }
+    const backToMainPage = document.createElement('a')
+    app2Div!.appendChild(backToMainPage)
+    backToMainPage.href = '/index.html'
+    backToMainPage.classList.add('btn')
+    backToMainPage.classList.add('btn-primary')
+    backToMainPage.textContent = 'Vissza a főoldalra'
 })
+
+/*
+<img src="" id="image" alt=""><br>
+        <ul>
+          <li>
+            <input type="radio" name="first" id="correct" disabled><label for="correct" id="correctLabel"></label><br>
+          </li>
+          <li>
+            <input type="radio" name="first" id="incorrect1" disabled><label for="incorrect1" id="incorrect1Label"></label><br>
+          </li>
+          <li>
+            <input type="radio" name="first" id="incorrect2" disabled><label for="incorrect2" id="incorrect2Label"></label><br>
+          </li>
+        </ul>
+*/
--- src/main.ts
+++ src/main.ts
@@ -19,15 +19,27 @@ async function getDatabase() {
   const incorrectInput1 = document.getElementById('incorrect1') as HTMLInputElement;
   const incorrectInput2 = document.getElementById('incorrect2') as HTMLInputElement;
   const error = document.getElementById('error');
+  const quizQuestions: string[] = [];
   const selectedData: string[] = [];
   let i = 1;
   image.src = currentQuiz[0].CountryImage;
   correctLabel!.textContent = currentQuiz[0].CorrectAnswer;
   incorrectLabel1!.textContent = currentQuiz[0].FirstIncorrectAnswer;
   incorrectLabel2!.textContent = currentQuiz[0].SecondIncorrectAnswer;
-  selectedData.push(currentQuiz[i].CorrectAnswer, currentQuiz[i].FirstIncorrectAnswer, currentQuiz[i].SecondIncorrectAnswer)
+  quizQuestions.push(currentQuiz[0].CountryImage ,currentQuiz[0].CorrectAnswer, currentQuiz[0].FirstIncorrectAnswer, currentQuiz[0].SecondIncorrectAnswer)
+  nextq!.addEventListener('click', () => {
+
+  }, { once: true});
+  if (correctInput.checked) {
+    selectedData.push(currentQuiz[0].CorrectAnswer)
+  } if (incorrectInput1.checked) {
+    selectedData.push(currentQuiz[0].FirstIncorrectAnswer)
+  } if (incorrectInput2.checked) {
+    selectedData.push(currentQuiz[0].SecondIncorrectAnswer)
+  }
   RandomOrder();
   nextq!.addEventListener('click', () => {
+    quizQuestions.push(currentQuiz[i].CountryImage, currentQuiz[i].CorrectAnswer, currentQuiz[i].FirstIncorrectAnswer, currentQuiz[i].SecondIncorrectAnswer)
     if (!correctInput.checked && !incorrectInput1.checked && !incorrectInput2.checked) {
       error!.textContent = "Kötelező kiválasztanod egy opciót!";
     } else {
@@ -35,11 +47,11 @@ async function getDatabase() {
         let points = parseInt(localStorage.getItem("points")!);
         points++;
         localStorage.setItem("points", points.toString())
-        selectedData.push(currentQuiz[i].CorrectAnswer, currentQuiz[i].FirstIncorrectAnswer, currentQuiz[i].SecondIncorrectAnswer)
+        selectedData.push(currentQuiz[i].CorrectAnswer)
       } if (incorrectInput1.checked) {
-        selectedData.push(currentQuiz[i].CorrectAnswer, currentQuiz[i].FirstIncorrectAnswer, currentQuiz[i].SecondIncorrectAnswer)
+        selectedData.push(currentQuiz[i].FirstIncorrectAnswer)
       } if (incorrectInput2.checked) {
-        selectedData.push(currentQuiz[i].CorrectAnswer, currentQuiz[i].FirstIncorrectAnswer, currentQuiz[i].SecondIncorrectAnswer)
+        selectedData.push(currentQuiz[i].SecondIncorrectAnswer)
       }
       console.log(selectedData)
       appDiv!.classList.toggle('w3-animate-bottom');
@@ -58,6 +70,7 @@ async function getDatabase() {
         nextq.textContent = "Kérdőív befejezése"
         nextq!.addEventListener('click', () => {
           localStorage.setItem("selectedOptions", JSON.stringify(selectedData));
+          localStorage.setItem("quizQuestions", JSON.stringify(quizQuestions));
           window.location.href = './end.html'
         })
       }
