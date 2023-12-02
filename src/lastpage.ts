0785817b1a385f62adc69507278c68ab78a90628
Dominik Zsolt Major <major.dominik.zsolt@petrik.hu>
2023-12-02 16:57:36 +0100

    utolsó oldal: eltalált adatok kiírásának elkezdése

--- src/lastpage.ts
+++ src/lastpage.ts
@@ -1,5 +1,15 @@
-
-
 const pointsParag = document.getElementById('points') as HTMLParagraphElement;
+const correctLabel = document.getElementById('correctLabel');
+const incorrectLabel1 = document.getElementById('incorrect1Label');
+const incorrectLabel2 = document.getElementById('incorrect2Label');
+const correctInput = document.getElementById('correct') as HTMLInputElement;
+const incorrectInput1 = document.getElementById('incorrect1') as HTMLInputElement;
+const incorrectInput2 = document.getElementById('incorrect2') as HTMLInputElement;
 let points = parseInt(localStorage.getItem("points")!);
-pointsParag.textContent = `Pontszámod: ${points}`
+let selectedData: string[] = JSON.parse(localStorage.getItem("selectedOptions")!);
+console.log(selectedData);
+pointsParag.textContent = `Pontszámod: ${points}/10`
+
+document!.addEventListener('DOMContentLoaded', () => {
+    
+})
--- src/main.ts
+++ src/main.ts
@@ -11,19 +11,21 @@ async function getDatabase() {
   const appDiv = document.getElementById('app');
   const app2Div = document.getElementById('app2');
   const correctLabel = document.getElementById('correctLabel');
-  const incorrectLabel1 = document.getElementById('incorrect1Label');
-  const incorrectLabel2 = document.getElementById('incorrect2Label');
+  const incorrectLabel1 = document.getElementById('incorrect1Label') as HTMLLabelElement
+  const incorrectLabel2 = document.getElementById('incorrect2Label') as HTMLLabelElement;
   const image = document.getElementById('image') as HTMLImageElement;
   const nextq = document.getElementById('nextq') as HTMLButtonElement;
   const correctInput = document.getElementById('correct') as HTMLInputElement;
   const incorrectInput1 = document.getElementById('incorrect1') as HTMLInputElement;
   const incorrectInput2 = document.getElementById('incorrect2') as HTMLInputElement;
   const error = document.getElementById('error');
+  const selectedData: string[] = [];
   let i = 1;
   image.src = currentQuiz[0].CountryImage;
   correctLabel!.textContent = currentQuiz[0].CorrectAnswer;
   incorrectLabel1!.textContent = currentQuiz[0].FirstIncorrectAnswer;
   incorrectLabel2!.textContent = currentQuiz[0].SecondIncorrectAnswer;
+  selectedData.push(currentQuiz[i].CorrectAnswer, currentQuiz[i].FirstIncorrectAnswer, currentQuiz[i].SecondIncorrectAnswer)
   RandomOrder();
   nextq!.addEventListener('click', () => {
     if (!correctInput.checked && !incorrectInput1.checked && !incorrectInput2.checked) {
@@ -33,8 +35,13 @@ async function getDatabase() {
         let points = parseInt(localStorage.getItem("points")!);
         points++;
         localStorage.setItem("points", points.toString())
-        console.log(points)
-      }      
+        selectedData.push(currentQuiz[i].CorrectAnswer, currentQuiz[i].FirstIncorrectAnswer, currentQuiz[i].SecondIncorrectAnswer)
+      } if (incorrectInput1.checked) {
+        selectedData.push(currentQuiz[i].CorrectAnswer, currentQuiz[i].FirstIncorrectAnswer, currentQuiz[i].SecondIncorrectAnswer)
+      } if (incorrectInput2.checked) {
+        selectedData.push(currentQuiz[i].CorrectAnswer, currentQuiz[i].FirstIncorrectAnswer, currentQuiz[i].SecondIncorrectAnswer)
+      }
+      console.log(selectedData)
       appDiv!.classList.toggle('w3-animate-bottom');
       app2Div!.classList.toggle('w3-animate-bottom');
       RandomOrder();
@@ -50,6 +57,7 @@ async function getDatabase() {
       if (i == 10) {
         nextq.textContent = "Kérdőív befejezése"
         nextq!.addEventListener('click', () => {
+          localStorage.setItem("selectedOptions", JSON.stringify(selectedData));
           window.location.href = './end.html'
         })
       }
